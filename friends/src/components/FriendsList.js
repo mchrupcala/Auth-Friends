c087a59d296e0d17b755a11bda5f5a4f044284d6
mchrupcala <michaelchrupcala@Michaels-MacBook-Pro.local>
2019-09-17 19:14:15 -0400

    Was able to get the new friend added and rendering on-screen. Added some styling too.

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,8 +1,19 @@
 import React, {useState, useEffect} from 'react';
 import axiosWithAuth from '../utils/axiosWithAuth';
+import egg from '../egg.png';
+// import axios from 'axios';
+
+console.log(egg);
 
 const FriendsList = () => {
     const [friends, setFriends] = useState([]);
+    const [newFriend, setNewFriend] = useState({
+        age: '',
+        email: '',
+        id: Date.now(), 
+        name: ''
+        // isPending: false
+    });
 
     useEffect(() => {
         axiosWithAuth().get('/friends')
@@ -16,31 +27,54 @@ const FriendsList = () => {
     }, [])
 
 
+    const handleChange = event => {
+        event.preventDefault();
+        setNewFriend({
+            ...newFriend,
+            [event.target.name]: event.target.value});
+    }
+
+    const submitHandler = event => {
+        // event.preventDefault();
+        axiosWithAuth().post('http://localhost:5000/api/friends', newFriend)
+        .then(res => {
+            console.log('Results from POST request: ', res);
+        })
+        .catch(err => {
+            console.log("Error! ", err)
+        })
+        // newFriend.isPending = true;
+    }
+
+
     return (
     <div>
         <h1>Welcome back, user!</h1>
         <h3>Do you want to make new friends?</h3>
 
-        <form>
+        <form className="friendslist-form" onSubmit={submitHandler}>
             Full Name:
-            <input type="text" name="name"  ></input>
+            <input type="text" name="name"  onChange={handleChange}></input>
 
             Age:
-            <input type="text" name="age"  ></input>
+            <input type="text" name="age"  onChange={handleChange}></input>
 
             Email: 
-            <input type="email" name="email"  ></input>
+            <input type="email" name="email"  onChange={handleChange}></input>
             <button type="submit">Add Friend</button>
         </form>
 
         {friends.map(item => {
             return (
-                <div key={item.id}>
-                    <h1>{item.name}</h1>
-                    <div>
-                        <p>Age: {item.age}</p>
-                        <p>{item.email}</p>
-                    </div>
+                <div className="one-friend" key={item.id}>
+                    <img src={egg} className="egg-image" />
+                        <div className="friend-card">
+                        <h1>{item.name}</h1>
+                        <div>
+                            <p>Age: {item.age}</p>
+                            <p>{item.email}</p>
+                        </div>
+                        </div>
                 </div>
             )
         })}
